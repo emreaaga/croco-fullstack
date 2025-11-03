b5b8ea6f1c4f9611e39369fa6adbdb42672e1119
Emre Agaliev <emreagaliev@gmail.com>
2025-11-03 13:05:10 +0300

    add JSDoc for user controllers

--- src/controllers/user.controolers.js
+++ src/controllers/user.controolers.js
@@ -1,5 +1,8 @@
 import { userService } from '../services/index.js';
 
+/**
+ * Get paginated list of users.
+ */
 export const getUsersController = async (request, response) => {
   const { data, pagination } = await userService.getAll(request?.validatedData);
   return response.status(200).json({
@@ -9,11 +12,17 @@ export const getUsersController = async (request, response) => {
   });
 };
 
+/**
+ * Change a user's status by ID.
+ */
 export const changeUserStatusController = async (request, response) => {
   await userService.updateStatus(request?.validatedData?.status, Number(request.params?.id));
   return response.status(200).json({ success: true, message: 'User status updated successfully.' });
 };
 
+/**
+ * Delete a user by ID.
+ */
 export const deleteUserController = async (request, response) => {
   await userService.delete(Number(request.params.id));
   return response.status(200).json({ success: true, message: 'Deleted successfully.' });

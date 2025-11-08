64c1cce845024a1d2b34ee6ceeb803fe0017129e
Emre Agaliev <emreagaliev@gmail.com>
2025-11-08 16:42:44 +0300

    chore: add paramValidationSchema export

--- src/validations/index.js
+++ src/validations/index.js
@@ -2,3 +2,4 @@ export { ApplicationSchema } from './application.validations.js';
 export { PaginateValidation } from './query.validations.js';
 export { RegisterSchema, LoginSchema, ChangePasswordSchema } from './auth.validations.js';
 export { UserPatchSchema } from './user.validations.js';
+export { paramValidationSchema } from './param.validation.js';
